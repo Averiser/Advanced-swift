d3856ad0182a8f8ee0e0d572a7c2c9684b4b9f4a
Averiser <averiser@gmail.com>
2021-11-24 23:30:28 +0300

    fifth commit - 299 Advanced Properties Challenge

--- ComputedProperties.playground/Contents.swift
+++ ComputedProperties.playground/Contents.swift
@@ -1,47 +1,23 @@
-import UIKit
+import Foundation
 
-var pizzaInInches: Int = 10 {
-  willSet {
-//    print(pizzaInInches)
-//    print(newValue)
-  }
-  didSet {
-    if pizzaInInches >= 18 {
-      print("Invalid size specified, pizzaInInches set to 18.")
-      pizzaInInches = 18
-    }
+var width: Float = 3.6
+var height: Float = 2.2
+
+var bucketsOfPaint: Int {
+  
+  get {
+    let area = width * height
+    let areaCoveredPerBucket: Float = 1.5
+    let numberOfBuckets = area / areaCoveredPerBucket
+    let roundedBuckets = ceilf(numberOfBuckets)
     
-//    print(oldValue)
-//    print(pizzaInInches)
-  }
-}
-
-pizzaInInches = 33
-print(pizzaInInches)
-
-
-var numberOfPeople: Int = 12
-let slicesPerPerson: Int = 4
-
-var numberOfSlices: Int {
-  get {
-    return pizzaInInches - 4
-  }
-//  set {
-//    print("numberOfSlices now has a new value which is \(newValue)")
-//  }
-}
-
-var numberOfPizza: Int {
-  get {
-    let numberOfPeopleFedPerPizza = numberOfSlices / slicesPerPerson
-    return numberOfPeople / numberOfPeopleFedPerPizza
+    return Int(roundedBuckets)
   }
   set {
-    let totalSlices = numberOfSlices * newValue
-    numberOfPeople = totalSlices / slicesPerPerson
+    let areaCanCover = Double(newValue) * 1.5
+    print("This amount of paint can cover an area of \(areaCanCover)")
   }
 }
-numberOfPizza = 4
 
-print(numberOfPeople)
+bucketsOfPaint = 8
+
